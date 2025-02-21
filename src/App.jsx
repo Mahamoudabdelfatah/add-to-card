13b8c414e1f1e7c17763155a6f739d8e855d1eb3
Mahmoudabdelfatah <mahmdmndbd@gmail.com>
2025-02-21 05:00:51 -0800

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,72 @@
 import React, { useState } from 'react'
 import Header from './components/Header'
-import Items from './components/items'
-
+import Items from './components/Items'
 
 const App = () => {
 
   const [products, setProducts] = useState([
-    { id: 1, name: "Chipsy", price: 10, count: 0 },
-    { id: 2, name: "Pepsi", price: 20, count: 0 },
-    { id: 3, name: "Cigarets", price: 30, count: 0 },
-    { id: 4, name: "Baneh", price: 40, count: 0 },
-
+    { id: 0, name: 'shipcy', price: 100, items: 1 },
+    { id: 1, name: 'pesi', price: 200, items: 1 },
+    { id: 2, name: 'molto', price: 300, items: 1 },
+    { id: 3, name: 'kranshy', price: 400, items: 1 },
+    { id: 4, name: 'cigaretes', price: 500, items: 1 }
   ])
 
+  const [addedProducts] = useState([
+    { id: 0, name: 'shipcy', price: 100, items: 1 },
+    { id: 1, name: 'pesi', price: 200, items: 1 },
+    { id: 2, name: 'molto', price: 300, items: 1 },
+    { id: 3, name: 'kranshy', price: 400, items: 1 },
+    { id: 4, name: 'cigaretes', price: 500, items: 1 }
+  ])
 
+  const [theme, setTheme] = useState(false)
+  const [showEmpty, setShowEmpty] = useState(false)
 
-  const Increment = (index) => {
-    let myProduct = structuredClone(products)
-    myProduct[index].count++
-    setProducts(myProduct)
-    calcTotalPiad()
+  const increment = (index) => {
+    setProducts(products.map((p) => (p.id === index ? { ...p, items: p.items + 1 } : p)))
   }
-
-  const Decrement = (index) => {
-    let myProduct = structuredClone(products);
-    myProduct[index].count--;
-    setProducts(myProduct)
-    calcTotalPiad()
+  const decrement = (index) => {
+    setProducts(products.map((p) => (p.id === index ? { ...p, items: p.items - 1 } : p)))
   }
-
-  const Delete = (index) => {
-    let myProduct = structuredClone(products);
-    myProduct.splice(index, 1);
+  const deletee = (index) => {
+    const myProduct = (products.filter((_, i) => i !== index));
     setProducts(myProduct)
-    calcTotalPiad()
     if (myProduct.length === 0) {
-      setShow(false)
+      setShowEmpty(true)
     }
   }
-
-  const countNav = products.reduce((total, product) => total + product.count, 0)
-
-  const [theme, setTheme] = useState(true);
-  const DarkLight = () => {
-    // if(theme==true){
-    //         setTheme(false)
-    //     }else{
-    //         setTheme(true)
-    //     }
-    theme ? setTheme(false) : setTheme(true)
-  }
-
-  const [show, setShow] = useState(true);
-  const DeleteCart = () => {
+  const deleteCarts = () => {
     setProducts([])
-    setShow(false)
-
+    setShowEmpty(true)
   }
-
-  const calcTotalPiad = () => {
-    return products.reduce((total, product) => total + (product.price * product.count), 0)
+  const darkLight = () => {
+    setTheme(!theme)
   }
-
-
-  const [mreset] = useState(products);
-  // let mreset = structuredClone(products)
   const reset = () => {
-    setProducts(mreset);
-    setShow(true)
-  };
+    setProducts(addedProducts)
+    setShowEmpty(false)
+  }
+
+  const totalPaid = () => {
+    return products.reduce((total, p) => total + (p.price * p.items), 0);
+  }
+
+  const counter = () => {
+    let count = 0;
+    products.forEach(product => {
+      if (product.items > 0) {
+        count += 1;
+      }
+    });
+    return count;
+  }
 
 
   return (
     <>
-      <Header count={countNav} />
-      <Items products={products} Increment={Increment} Decrement={Decrement} Delete={Delete} theme={theme} DarkLight={DarkLight} DeleteCart={DeleteCart} show={show} totalPiad={calcTotalPiad()} reset={reset} />
-
+      <Header addedProducts={addedProducts} products={products} increment={increment} counter={counter} theme={theme} />
+      <Items products={products} increment={increment} decrement={decrement} deletee={deletee} deleteCarts={deleteCarts} darkLight={darkLight} reset={reset} theme={theme} totalPaid={totalPaid} showEmpty={showEmpty} />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,40 @@
 import React from 'react'
 import img from "../assets/Me.jpg"
 
+const Header = ({ addedProducts, products, increment, counter,theme }) => {
 
-const Header = ({ count }) => {
+
     return (
-
-        <div className="navbar bg-neutral text-neutral-content shadow-sm font-bold p-4 fixed h-[1.2em] fi">
+        <div className={`${theme ? "bg-black" : "bg-white"}  navbar bg-[#005f78] bg-opacity-75 text-neutral-content shadow-sm font-bold p-4 fixed w-full top-0 z-50`}>
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">Shop</a>
+                <div className="flex justify-center items-center gap-3 flex-wrap ">
+                    {addedProducts.map((product, index) => (
+                        <button
+                            key={product.id}
+                            onClick={() => increment(product.id)}
+                            className={`btn btn-soft btn-secondary `}
+                        >
+                            {product.name}
+                        </button>
+                    ))}
+                </div>
             </div>
+
             <div className="flex-none">
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                         <div className="indicator">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
-                            <span className="badge badge-sm indicator-item">{count}</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                            </svg>
+                            <span className="badge badge-sm indicator-item">{counter()}</span>
                         </div>
                     </div>
                     <div
                         tabIndex={0}
                         className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
                         <div className="card-body">
-                            <span className="text-lg font-bold">{count} Items</span>
-
+                            <span className="text-lg font-bold">{counter()} Items</span>
                         </div>
                     </div>
                 </div>
@@ -30,15 +42,14 @@ const Header = ({ count }) => {
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
                             <img
-                                alt="Tailwind CSS Navbar component"
-                                src={img} />
+                                alt="User Avatar"
+                                src={img}
+                            />
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
-
     )
 }
 
